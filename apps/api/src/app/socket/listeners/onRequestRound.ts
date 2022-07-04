c97cdd03a78279770b493f49f1eba8055a76da86
Behemoth <simonostini@gmail.com>
2022-07-04 15:29:47 +0200

    update

--- apps/api/src/app/socket/listeners/onRequestRound.ts
+++ apps/api/src/app/socket/listeners/onRequestRound.ts
@@ -1,6 +1,8 @@
 import {
   defaultExplainTime,
   defaultRoundsAmount,
+  Player,
+  Question,
   Round,
 } from '@convinz/shared/types';
 import { getRandomQuestion } from '@convinz/shared/util';
@@ -14,31 +16,22 @@ export const onRequestRound: Listener = (socket) => {
     const question = getRandomQuestion();
     const playerWithSolution = lobby.players.getRandom();
 
-    // const broadcastNewRound = () => {};
-
-    if (
-      !lobby.currentRound ||
-      lobby.currentRound.submissions.length >= lobby.players.length
-    ) {
-      lobby.roundHistory.push(new Round(question));
-    } else return;
-
-    lobby.voteTimerExpiringEmitter.on('expired', () => {
-      console.log(lobby.roundHistory);
-      if (lobby.roundHistory.length >= defaultRoundsAmount) {
-        io.to(gameCode).emit('gameEnded', {
-          gameCode,
-          gameResults: lobby.getTotalVotesPerPlayer(),
-        });
-        return;
+    const broadcastNewRound = (
+      newQuestion: Question,
+      playerWithSolution: Player
+    ) => {
+      if (!lobby.currentActionTimerInterval) {
+        lobby.currentActionTimerInterval = setInterval(() => {
+          io.to(gameCode).emit('explainTimerTickExpired', {
+            ...lobby.explainTimer,
+          });
+          lobby.decrementExplainTimeLeft();
+          if (lobby.explainTimer.timeLeft === -1) {
+            lobby.clearCurrentActionTimerInterval();
+            console.log('time over');
+          }
+        }, 1000);
       }
-
-      lobby.clearCurrentActionTimerInterval();
-
-      const newQuestion = getRandomQuestion();
-      lobby.roundHistory.push(new Round(newQuestion));
-
-      const playerWithSolution = lobby.players.getRandom();
 
       // answer to player with solution
       io.to(playerWithSolution.id).emit('receivedRound', {
@@ -55,46 +48,33 @@ export const onRequestRound: Listener = (socket) => {
         solution: null,
         totalTime: defaultExplainTime,
       });
+    };
 
-      lobby.currentActionTimerInterval = setInterval(() => {
-        io.to(gameCode).emit('explainTimerTickExpired', {
-          ...lobby.explainTimer,
+    if (
+      !lobby.currentRound ||
+      lobby.currentRound.submissions.length >= lobby.players.length
+    ) {
+      lobby.roundHistory.push(new Round(question));
+    } else return;
+
+    lobby.voteTimerExpiringEmitter.on('expired', () => {
+      if (lobby.roundHistory.length >= defaultRoundsAmount) {
+        io.to(gameCode).emit('gameEnded', {
+          gameCode,
+          gameResults: lobby.getTotalVotesPerPlayer(),
         });
-        lobby.decrementExplainTimeLeft();
-        if (lobby.explainTimer.timeLeft === -1) {
-          lobby.clearCurrentActionTimerInterval();
-          console.log('time over');
-        }
-      }, 1000);
+        return;
+      }
+
+      const newQuestion = getRandomQuestion();
+      const playerWithSolution = lobby.players.getRandom();
+      lobby.roundHistory.push(new Round(newQuestion));
+
+      lobby.clearCurrentActionTimerInterval();
+
+      broadcastNewRound(newQuestion, playerWithSolution);
     });
 
-    if (!lobby.currentActionTimerInterval) {
-      lobby.currentActionTimerInterval = setInterval(() => {
-        io.to(gameCode).emit('explainTimerTickExpired', {
-          ...lobby.explainTimer,
-        });
-        lobby.decrementExplainTimeLeft();
-        if (lobby.explainTimer.timeLeft === -1) {
-          lobby.clearCurrentActionTimerInterval();
-          console.log('time over');
-        }
-      }, 1000);
-    }
-
-    // answer to player with solution
-    io.to(playerWithSolution.id).emit('receivedRound', {
-      gameCode,
-      question: question.question,
-      solution: question.solution,
-      totalTime: defaultExplainTime,
-    });
-
-    // answer to all other players
-    io.to(gameCode).except(playerWithSolution.id).emit('receivedRound', {
-      gameCode,
-      question: question.question,
-      solution: null,
-      totalTime: defaultExplainTime,
-    });
+    broadcastNewRound(question, playerWithSolution);
   });
 };
